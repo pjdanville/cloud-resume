4f155765deffe966a4945e83ff55f22a575c5976
pjdanville <79735815+pjdanville@users.noreply.github.com>
2023-12-07 17:49:23 -0500

    Version 1: html,css,and js files

---
+++ project.js
@@ -0,0 +1,8 @@
+// const counter = document.querySelector(".counter-number");
+// async function updateCounter() {
+//     let response = await fetch("https://etdbi37vznpbs5sqlc6cu6bliy0wwhte.lambda-url.us-east-1.on.aws/");
+//     let data = await response.json();
+//     counter.innerHTML = 'Views: ${data} ';
+// }
+
+// updateCounter();
